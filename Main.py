813722ca3391e0e6c22bb50ff0023830d93958e3
Zdeněk Němec <xzdeneknemec@outlook.cz>
2022-05-07 09:14:26 +0200

    init

---
+++ Main.py
@@ -0,0 +1,11 @@
+def select_sort(numbers):
+    for x in range(len(numbers)):
+        min_index = x
+
+        for i in range(x+1, len(numbers)):
+            if numbers[i] < numbers[min_index]:
+                min_index = i
+
+        numbers[x], numbers[min_index] = numbers[min_index], numbers[x]
+
+    return numbers
